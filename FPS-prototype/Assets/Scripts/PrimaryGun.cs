d7aff1bd7fd00a11c3c67d904626606be133d8e8
Iñaki Diez Galarza <inakidiezgalarza@gmail.com>
2020-05-03 19:00:27 -0300

    Lograda base: assets de armas y disparo con raycast

---
+++ FPS-prototype/Assets/Scripts/PrimaryGun.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrimaryGun : MonoBehaviour
+{
+    public float rayDistance = 10;
+    AudioSource gunfire;
+    Animation recoil;
+    void Start()
+    {
+        gunfire = GetComponent<AudioSource>();
+        recoil = GetComponent<Animation>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            gunfire.Play();
+            recoil.Play("GunShot");
+        }
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, transform.forward, out hit, rayDistance))
+        {
+            Debug.DrawRay(transform.position, transform.forward * hit.distance, Color.yellow);
+
+            if (Input.GetKeyDown(KeyCode.Mouse0)&&hit.transform.gameObject.tag=="Enemy")
+            {
+                Destroy(hit.transform.gameObject);
+            }
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, transform.forward * rayDistance, Color.white);
+        }
+    }
+}
